65d3ae9be3e5a652fbc6b0f8716638218a798a7d
Gustavo Rocha <97960688+gu-rocha@users.noreply.github.com>
2022-06-01 13:58:51 -0300

    Add files via upload

---
+++ F to C.js
@@ -0,0 +1,25 @@
+transformDegree('50F')
+function transformDegree(degree) {
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    if(!celsiusExists && !fahrenheitExists) {
+        throw new Error ('Grau não identificado')
+    }
+
+    // Fluxo ideal, F -> C
+    let updateDegree = Number (degree.toUpperCase().replace('F', ""));
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+    let degreeSign = 'C'
+
+    // Fluxo alternativo, C -> F
+    if(celsiusExists) {
+        updateDegree = Number (degree.toUpperCase().replace('C', ""));
+        formula = celsius => celsius * 9/5 + 32
+        degreeSign = 'F'
+    }
+
+
+    return formula(updateDegree) + degreeSign
+
+}
